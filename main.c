bf951e63714315905604fde241fe42c24b219865
Kocheshvili <Luxi@mail.ru>
2015-05-13 15:08:15 +0300

    Update main.c

--- main.c
+++ main.c
@@ -11,7 +11,8 @@ int main(int argc, char **argv)
 
     printf("Now enter your last name: ");
     gets(last); /* buffer overflow? what's that? */
-	printf("Hello", first, last);
+	//printf("Hello", first, last);
+	
     printf("Hello %s %s!\n", first, last);
     return 0;
 }
